5bcd9f3d82fe95c875e4d5b8423681ca461c6d7a
MeeetShaaah <meetshah7199@gmail.com>
2024-07-22 16:26:54 -0400

    Start Product Microservice with Spring_Boot & kafka.

---
+++ ProductMicroservice/src/test/java/com/shreyraj/ProductMicroservice/ProductMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyraj.ProductMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
