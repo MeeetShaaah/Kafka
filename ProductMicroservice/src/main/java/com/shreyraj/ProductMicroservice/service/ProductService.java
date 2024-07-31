fe3dd7e8f6f37bac925781c6a47500869d734e1e
MeeetShaaah <meetshah7199@gmail.com>
2024-07-31 14:02:35 -0400

    NA

--- ProductMicroservice/src/main/java/com/shreyraj/ProductMicroservice/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.shreyraj.ProductMicroservice.service;
-
-import com.shreyraj.ProductMicroservice.rest.CreateProductRestModel;
-
-public interface ProductService {
-    String createProduct(CreateProductRestModel CreateProductRestModel) throws Exception;
-}
